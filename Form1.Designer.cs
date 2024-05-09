23e36fcc6bff16b698bb6e0584e7743b4c9bb672
itsmecyril <cyrilypil@gmail.com>
2024-05-10 00:15:43 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace FixedPartitionSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.memoryRAMBox = new System.Windows.Forms.TextBox();
+            this.noProcessesBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.processID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.memoryReq = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.alloctime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.completiontime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.memoryRAMPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(105, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fixed Partition Simulator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(109, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Memory(RAM) Size:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(346, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter number of Processes:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(249, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Proceed Table";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // memoryRAMBox
+            // 
+            this.memoryRAMBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.memoryRAMBox.Location = new System.Drawing.Point(112, 156);
+            this.memoryRAMBox.Name = "memoryRAMBox";
+            this.memoryRAMBox.Size = new System.Drawing.Size(134, 26);
+            this.memoryRAMBox.TabIndex = 6;
+            // 
+            // noProcessesBox
+            // 
+            this.noProcessesBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.noProcessesBox.Location = new System.Drawing.Point(349, 156);
+            this.noProcessesBox.Name = "noProcessesBox";
+            this.noProcessesBox.Size = new System.Drawing.Size(134, 26);
+            this.noProcessesBox.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(270, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "00:00";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.processID,
+            this.memoryReq,
+            this.alloctime,
+            this.completiontime});
+            this.dataGridView1.Location = new System.Drawing.Point(93, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(440, 283);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // processID
+            // 
+            this.processID.HeaderText = "Process ID";
+            this.processID.Name = "processID";
+            // 
+            // memoryReq
+            // 
+            this.memoryReq.HeaderText = "Memory Requirement";
+            this.memoryReq.Name = "memoryReq";
+            // 
+            // alloctime
+            // 
+            this.alloctime.HeaderText = "Allocation Time";
+            this.alloctime.Name = "alloctime";
+            // 
+            // completiontime
+            // 
+            this.completiontime.HeaderText = "Completion Time";
+            this.completiontime.Name = "completiontime";
+            // 
+            // memoryRAMPanel
+            // 
+            this.memoryRAMPanel.BackColor = System.Drawing.Color.Khaki;
+            this.memoryRAMPanel.Location = new System.Drawing.Point(590, 80);
+            this.memoryRAMPanel.Name = "memoryRAMPanel";
+            this.memoryRAMPanel.Size = new System.Drawing.Size(100, 300);
+            this.memoryRAMPanel.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(590, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(101, 34);
+            this.panel1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "OS 50KB";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(716, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 24);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Reset Computer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label6.Location = new System.Drawing.Point(132, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "(1000KB - 5000KB)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Location = new System.Drawing.Point(370, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "(5-10 Processes)";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(888, 519);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.memoryRAMPanel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.noProcessesBox);
+            this.Controls.Add(this.memoryRAMBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fixed Partition Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox memoryRAMBox;
+        private System.Windows.Forms.TextBox noProcessesBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn processID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn memoryReq;
+        private System.Windows.Forms.DataGridViewTextBoxColumn alloctime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn completiontime;
+        private System.Windows.Forms.FlowLayoutPanel memoryRAMPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixedPartitionSimulation
+{
+    public partial class Form1 : Form
+    {
+        private int timer = 0;
+        private const int kernel = 50;
+        private int RAM, processes; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer++;
+            label4.Text = timer.ToString();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        // Proceed to Table Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (memoryRAMBox.Text == "" && noProcessesBox.Text == "")
+                MessageBox.Show("Fields cannot be empty", "Fixed Partition Simulator", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                int memoryRAM = Convert.ToInt32(memoryRAMBox.Text);
+                int noProcesses = Convert.ToInt32(noProcessesBox.Text);
+                if((memoryRAM > 5000 || memoryRAM <= 0 || memoryRAM < 1000) || (noProcesses > 10 || noProcesses <= 0 || noProcesses < 5))
+                    MessageBox.Show("Invalid input", "Fixed Partition Simulator", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    dataGridView1.Show();
+                    fillTable(noProcesses);
+                    configureMemoryPartitions(memoryRAM, noProcesses);
+                }
+            }
+            
+
+        }
+        // Allocate the Process ID's in the table
+        private void fillTable(int processes)
+        {
+            for (int i = 0; i < processes; i++)
+            {
+                string processId = $"P{i + 1}"; // Generate process IDs (P1, P2, ...)
+                dataGridView1.Rows.Add(processId); // Add a new row with the process ID
+            }
+        }
+        private void configureMemoryPartitions(int memoryRAM, int noProcesses)
+        {
+            memoryRAMPanel.Margin = new Padding(0); // Set zero margin for no spacing
+            memoryRAMPanel.Padding = new Padding(0);
+            double panelHeight = memoryRAMPanel.Height;
+            double panelWidth = memoryRAMPanel.Width;
+            double heightOfPanels = panelHeight / noProcesses;
+            double memoryUsable = memoryRAM - 50;
+            decimal partitionSizes = (decimal)memoryUsable / (decimal)noProcesses;
+
+            for (int i = 0; i < noProcesses; i++)
+            {
+                Panel panel = new Panel();
+                panel.Size = new Size((int)panelWidth, (int)heightOfPanels);
+                panel.BorderStyle = BorderStyle.FixedSingle;
+                panel.Margin = new Padding(0);
+                Label partitionLabel = new Label();
+                partitionSizes = Math.Round(partitionSizes, 2);
+                partitionLabel.Text = $"{partitionSizes} KB";
+                partitionLabel.AutoSize = true;
+                partitionLabel.Dock = DockStyle.Fill;
+                panel.Controls.Add(partitionLabel);
+                memoryRAMPanel.Controls.Add(panel);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Reset Computer Button to Reconfigure new Partitions for the Memory(RAM)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            memoryRAMPanel.Controls.Clear();
+            dataGridView1.Rows.Clear();
+            memoryRAMBox.Clear();
+            noProcessesBox.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Hide();
+        }
+    }
+}
